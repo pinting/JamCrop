ad986436e1e1e91fec744fa91e04ccbd4640b7af
pinting <tornyi.denes@gmail.com>
2013-01-18 18:26:27 +0100

    Tooltip class, and some change

--- crop.py
+++ crop.py
@@ -7,11 +7,8 @@ APP_KEY     = '***REMOVED***'
 APP_SECRET  = '***REMOVED***'
 
 import Tkinter, xml.etree.ElementTree as xml, time, os, webbrowser, tkMessageBox
-from dropbox import client, rest, session
+from dropbox import client, session
 from pil import ImageGrab
-
-configHandler = xml.parse(CONFIG)
-configRoot = configHandler.getroot()
 
 class reference():
     def __init__(self, var):
@@ -30,9 +27,9 @@ class dropbox(session.DropboxSession):
     def link(self, token):
         """ Create a new link between Dropbox and JamCrop """
         self.obtain_access_token(token)
-        configRoot.find('token').text = "|".join([self.token.key, self.token.secret])
+        config.getroot().find('token').text = "|".join([self.token.key, self.token.secret])
         
-    def auth(self):
+    def authenticate(self):
         """ Get the verification link from Dropbox """
         token = self.obtain_request_token()
         webbrowser.open(self.build_authorize_url(token))
@@ -40,14 +37,14 @@ class dropbox(session.DropboxSession):
 
     def unlink(self):
         """ Unlink the JamCrop from Dropbox """
-        configRoot.find('token').text = None
+        config.getroot().find('token').text = None
         session.DropboxSession.unlink(self)
         
     def load(self):
         """ Load the saved link from token """
-        if(configRoot.find('token').text != None):
+        if(config.getroot().find('token').text != None):
             try:
-                self.set_token(*configRoot.find('token').text.split('|'))
+                self.set_token(*config.getroot().find('token').text.split('|'))
                 return(1)
             except: pass
         return(0)
@@ -87,10 +84,26 @@ class window():
         check.place(x = x, y = y)
         return(check)
         
+class tooltip(Tkinter.Toplevel, window):
+    def __init__(self, parent, message, timeout = 1500):
+        """ Initializing the config window """
+        Tkinter.Toplevel.__init__(self, parent, bg = 'black')
+        self.overrideredirect(1)
+        self.geometry("%dx%d%+d%+d" % (128, 24, self.winfo_screenwidth()-128, 0))
+        self.resizable(width = 'false', height = 'false')
+        self.attributes('-alpha', 0.75)
+        self.wm_attributes("-topmost", 1)
+        
+        self.bind("<Button-1>", lambda event: self.destroy())
+        self.after(timeout, self.destroy)
+        
+        message = self.label(self, 5, 0, message)
+        message.config(bg = "black", fg = "white")
+        
 class configWindow(Tkinter.Toplevel, window):
     def __init__(self, parent, session, status = reference(0)):
-        """ Initializing the config windows """
-        Tkinter.Toplevel.__init__(self, parent, width = 195, height = 110)
+        """ Initializing the config window """
+        Tkinter.Toplevel.__init__(self, parent, width = 195, height = 135)
         self.protocol('WM_DELETE_WINDOW', lambda: self.deleteEvent(status))
         self.resizable(width = 'false', height = 'false')
         self.attributes('-toolwindow', 1)
@@ -103,24 +116,31 @@ class configWindow(Tkinter.Toplevel, window):
         copyValue = Tkinter.StringVar()
         copyCheck = self.check(self, 172, 5, copyValue, 'true', 'false')
         copyValue.trace(mode = 'w', callback = lambda varName, elementName, mode: self.setCopy(varName, elementName, mode, copyValue))
-        if(configRoot.find('copy').text == 'false'): copyCheck.deselect()
+        if(config.getroot().find('copy').text == 'false'): copyCheck.deselect()
 		
-        # Create the browser behavor checkbox
+        # Create the browser behavior checkbox
         self.label(self, 5, 30, "Open in the browser:")
         shortValue = Tkinter.StringVar()
         shortCheck = self.check(self, 172, 30, shortValue, 'true', 'false')
         shortValue.trace(mode = 'w', callback = lambda varName, elementName, mode: self.setBrowser(varName, elementName, mode, shortValue))
-        if(configRoot.find('browser').text == 'false'): shortCheck.deselect()
+        if(config.getroot().find('browser').text == 'false'): shortCheck.deselect()
+		
+        # Create the tooltip checkbox
+        self.label(self, 5, 55, "Show a tooltip:")
+        tooltipValue = Tkinter.StringVar()
+        tooltipCheck = self.check(self, 172, 55, tooltipValue, 'true', 'false')
+        tooltipValue.trace(mode = 'w', callback = lambda varName, elementName, mode: self.setTooltip(varName, elementName, mode, tooltipValue))
+        if(config.getroot().find('tooltip').text == 'false'): tooltipCheck.deselect()
         
         # Create the list of image formats
-        self.label(self, 5, 55, "Image format:")
+        self.label(self, 5, 80, "Image format:")
         formatValue = Tkinter.StringVar()
-        formatValue.set(configRoot.find('format').text)
-        formatMenu = self.menu(self, 93, 50, 9, formatValue, ['jpg', 'png', 'gif'])
+        formatValue.set(config.getroot().find('format').text)
+        formatMenu = self.menu(self, 93, 75, 9, formatValue, ['jpg', 'png', 'gif'])
         formatValue.trace(mode = 'w', callback = lambda varName, elementName, mode: self.setFormat(varName, elementName, mode, formatValue))
         
-        # Create the button for unlink the applcation
-        button = self.button(self, 5, 80, 25, text = "Unlink JamCrop")
+        # Create the button to unlink the application
+        button = self.button(self, 5, 105, 25, text = "Unlink from Dropbox")
         button.bind("<Button-1>", lambda event: self.doUnlink(event, parent, session))
         
         status.set(1)
@@ -132,18 +152,22 @@ class configWindow(Tkinter.Toplevel, window):
         
     def setCopy(self, varName, elementName, mode, value):
         """ Set the url copy paramter """
-        configRoot.find('copy').text = unicode(value.get())
+        config.getroot().find('copy').text = unicode(value.get())
         
     def setBrowser(self, varName, elementName, mode, value):
         """ Set the browser parameter """
-        configRoot.find('browser').text = unicode(value.get())
+        config.getroot().find('browser').text = unicode(value.get())
+        
+    def setTooltip(self, varName, elementName, mode, value):
+        """ Set the tooltop parameter """
+        config.getroot().find('tooltip').text = unicode(value.get())
         
     def setFormat(self, varName, elementName, mode, value):
         """ Change the format parameter """
-        configRoot.find('format').text = unicode(value.get())
+        config.getroot().find('format').text = unicode(value.get())
         
     def doUnlink(self, event, parent, session):
-        """ Unlink JamCrop from Dropbox, and close the windows """
+        """ Unlink JamCrop from Dropbox, and close the window """
         session.unlink()
         parent.deleteEvent()
         
@@ -153,7 +177,7 @@ class grabWindow(Tkinter.Tk):
     square, session = None, None
 
     def __init__(self): 
-        """ Initializing the grab windows of the application """
+        """ Initializing the grab window of the application """
         Tkinter.Tk.__init__(self)
         self.protocol('WM_DELETE_WINDOW', self.deleteEvent)
         self.configure(background = 'white')
@@ -171,8 +195,8 @@ class grabWindow(Tkinter.Tk):
         if(not self.session.load()):
             self.withdraw()
             
-            token = self.session.auth()
-            if(tkMessageBox.askokcancel(title = "JamCrop", message = "The JamCrop require a limited Dropbox access for itself. If you allowed the connection to the Dropbox, from the recently appeared browser window, please click on the OK button. After the grab windows have shown, you can open the configuration windows by pressing the F1 button.")):
+            token = self.session.authenticate()
+            if(tkMessageBox.askokcancel(title = "JamCrop", message = "The JamCrop require a limited Dropbox access for itself. If you allowed the connection to the Dropbox, from the recently appeared browser window, please click on the OK button. After the grab window have shown, you can open the configuration window by pressing the F1 button.")):
                 try: self.session.link(token)
                 except: return
             else: return
@@ -182,7 +206,7 @@ class grabWindow(Tkinter.Tk):
         
     def deleteEvent(self):
         """ Closing function for the grab window """
-        configHandler.write(CONFIG)
+        config.write(CONFIG)
         self.destroy()
     
     def autoFocus(self):
@@ -196,7 +220,7 @@ class grabWindow(Tkinter.Tk):
         self.after(250, self.autoFocus)
     
     def keyPress(self, event):
-        """ Parse the key presses """
+        """ Track the key presses """
         if(event.keysym == 'Escape'):
             self.deleteEvent()
         elif(event.keysym == 'F1' and not self.disabled.get()):
@@ -246,17 +270,21 @@ class grabWindow(Tkinter.Tk):
         self.withdraw()
         connection = client.DropboxClient(self.session)
         
-        fileName = time.strftime('%Y_%m_%d_%H_%M_%S') + '.' + configRoot.find('format').text;
+        fileName = time.strftime('%Y_%m_%d_%H_%M_%S') + '.' + config.getroot().find('format').text;
         ImageGrab.grab((x1, y1, x2, y2)).save(fileName)
         connection.put_file(fileName, open(os.path.expanduser(fileName), "rb"))
         os.unlink(fileName)
         result = connection.share(fileName)
         
-        if(configRoot.find('browser').text == 'true'):
-			webbrowser.open(result['url'])
-        if(configRoot.find('copy').text == 'true'):
+        if(config.getroot().find('tooltip').text == 'true'):
+            alert = tooltip(self, "The uploading is completed", 2000)
+            alert.geometry("%dx%d%+d%+d" % (160, 25, self.winfo_screenwidth()-160, 0))
+            alert.mainloop()
+        if(config.getroot().find('copy').text == 'true'):
             self.clipboard_clear()
             self.clipboard_append(result['url'])
+        if(config.getroot().find('browser').text == 'true'):
+			webbrowser.open(result['url'])
         
         self.deleteEvent()
 
@@ -266,4 +294,5 @@ def main():
     crop.mainloop()
 
 if(__name__ == '__main__'):
+    config = xml.parse(CONFIG)
     main()
